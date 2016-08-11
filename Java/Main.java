3a7077e9893ead6a7dd2a256701a48da97e53a51
chibi929 <anbi929.dev@gmail.com>
2016-08-11 23:55:19 +0900

    Add a directory 'Java'.

---
+++ Java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.println("Hello World!");
+        hoge();
+        Sample sample = new Sample();
+        sample.test();
+        n = 2;
+    }
+
+    private static void hoge() {
+        System.out.println("hoge");
+    }
+}
---
+++ Java/Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public Sample() {
+        System.out.println("Sample");
+    }
+
+    public void test() {
+        System.out.println("test");
+    }
+}
