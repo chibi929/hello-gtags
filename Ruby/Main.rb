a334d6f2bc1ee138dfeb0809ecfc7d7323e42277
chibi929 <anbi929.dev@gmail.com>
2016-08-11 23:52:45 +0900

    Add a directory 'Ruby'.

---
+++ Ruby/Main.rb
@@ -0,0 +1,12 @@
+require './Sample.rb'
+
+def hoge
+  puts "hoge"
+end
+
+n = 0;
+puts "Hello World!"
+hoge()
+sample = Sample.new
+sample.test()
+n = 2;
---
+++ Ruby/Sample.rb
@@ -0,0 +1,5 @@
+class Sample
+  def test
+    puts "test"
+  end
+end
